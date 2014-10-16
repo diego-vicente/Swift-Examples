0dbee1a60f38c4dc130156ebfc9d69252e52dc4e
Diego Vicente <diegovicentemartin@me.com>
2014-10-16 20:54:24 +0200

    Added examples of enum and struct
    Added enums Rank and Suit and struct Card, and with their respective
    methods

--- Swift-Examples.swift
+++ Swift-Examples.swift
@@ -47,7 +47,7 @@ class Circle: Shape {
         self.name = name
     }
 
-    func area () -> Double {
+    func area() -> Double {
         return 6.28*radius
     }
 }
@@ -68,5 +68,84 @@ class EquilateralTriangle: Shape {
         set {
             sideLength = newValue / 3.0
         }
+    }
+}
+
+/* Examples of enum */
+
+enum Rank: Int {
+    case Ace = 1
+    case Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten
+    case Jack, Queen, King
+
+    func simpleDescription() -> String {
+        switch self {
+            case .Ace:
+                return "ace"
+            case .Jack:
+                return "jack"
+            case .Queen:
+                return "queen"
+            case .King:
+                return "king"
+            default:
+                return String(self.toRaw())
+        }
+    }
+
+    func comparedTo(Rank other) -> Rank {
+        if (self.toRaw()<other.toRaw()){
+            return other
+        }
+        else if (self.toRaw()>other.toRaw()){
+            return self
+        }
+        else {
+            println("Both cards are equal")
+        }
+    }
+}
+
+enum Suit: Int {
+    case Spades = 1, Hearts, Diamonds, Clubs
+
+    func simpleDescritption() -> String {
+        switch self {
+            case .Spades:
+                return "spades"
+            case .Hearts:
+                return "hearts"
+            case .Diamonds:
+                return "diamonds"
+            case .Clubs:
+                return "clubs"
+        }
+    }
+
+    func color() - String {
+        switch self {
+        case .Spades, .Clubs:
+            return "black"
+        case .Hearts, .Diamonds:
+            return "red"
+    }
+}
+
+/* Example of struct */
+struct Card {
+    var rank: Rank
+    var suit: Suit
+
+    func simpleDescription() -> String {
+        return "The \(rank.simpleDescription()) of \(suit.simpleDescription())"
+    }
+
+    func initializeDeck() -> Card[]{
+        let deck: Card = [:]
+        for i in 1...4 {
+            for j in 1...13 {
+                let card = Card(rank: .fromRaw(j), suit: .fromRaw(i))
+            }
+        return deck
     }
 }
